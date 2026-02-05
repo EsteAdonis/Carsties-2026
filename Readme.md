a726e83f3836f96559ec1e3765e96a45afa629f1
EsteAdonis <julio.e.ochoa.p@gmail.com>
2026-02-04 23:16:59 -0600

    AuctionService

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Carsties 
+
+### Getting PostgresSQL Image based on compose.yaml
+docker compose up -d
---
+++ src/AuctionService/Data/Migrations/20260204041711_Fixing Column Seller of Auctions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingColumnSellerofAuctions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sellect",
+                table: "Auctions",
+                newName: "Seller");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Seller",
+                table: "Auctions",
+                newName: "Sellect");
+        }
+    }
+}
